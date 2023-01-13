b2e2c4793258d8a74735423c1b7fc04a27f3f2c5
Nurullah1453 <nurullah.demir1453@gmail.com>
2023-01-13 11:29:18 +0300

    13.01.2023 cucumber calismalarim

--- src/test/java/stepdefinitions/WebUniversityStepdefinitions.java
+++ src/test/java/stepdefinitions/WebUniversityStepdefinitions.java
@@ -1,7 +1,11 @@
 package stepdefinitions;
 
 import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.interactions.Actions;
+import pages.WebUniversityPage;
+import utilities.ConfigReader;
 import utilities.Driver;
 
 import java.util.Set;
@@ -12,7 +16,8 @@ public class WebUniversityStepdefinitions {
 
     @Then("Login Portal elementine kadar asagi iner")
     public void login_portal_elementine_kadar_asagi_iner() {
-
+        Actions actions=new Actions(Driver.getDriver());
+        actions.sendKeys(Keys.PAGE_DOWN).perform();
     }
     @Then("Login Portal a tiklar")
     public void login_portal_a_tiklar() {
@@ -40,31 +45,34 @@ public class WebUniversityStepdefinitions {
     }
     @Then("username kutusuna deger yazar")
     public void username_kutusuna_deger_yazar() {
-
+        webUniversityPage.userNameKutusu.sendKeys(ConfigReader.getProperty("wduUsernameDeger"));
     }
     @Then("password kutusuna deger yazar")
     public void password_kutusuna_deger_yazar() {
-
+        webUniversityPage.passwordKutusu.sendKeys(ConfigReader.getProperty("wduPasswordDeger"));
     }
     @Then("webUniversity login butonuna basar")
     public void web_university_login_butonuna_basar() {
-
+        webUniversityPage.loginButonu.click();
     }
     @Then("Popup’ta cikan yazinin validation failed oldugunu test eder")
     public void popup_ta_cikan_yazinin_validation_failed_oldugunu_test_eder() {
+        String actualYazı=Driver.getDriver().switchTo().alert().getText();
+        String expectedYazı="validation failed";
+        Assert.assertTrue(actualYazı.equals(expectedYazı));
 
     }
     @Then("Ok diyerek Popup’i kapatir")
     public void ok_diyerek_popup_i_kapatir() {
-
+        Driver.getDriver().switchTo().alert().accept();
     }
     @Then("Ilk sayfaya geri doner")
     public void ılk_sayfaya_geri_doner() {
-
+        Driver.getDriver().switchTo().window(ilkSayfaWHD);
     }
     @Then("Ilk sayfaya donuldugunu test eder")
     public void ılk_sayfaya_donuldugunu_test_eder() {
-
+        Assert.assertTrue(webUniversityPage.contactUsLinki.isDisplayed());
     }
 
 }
